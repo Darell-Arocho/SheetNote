157ced28cdbe32af74add0fb04f01964cb0450f4
Darell Arocho <darell.arocho7@gmail.com>
2023-04-19 11:52:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sketch_1/CanvasView.java
@@ -0,0 +1,109 @@
+package com.example.sketch_1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CanvasView extends View
+{
+    Context context;
+        int width, height;
+        Bitmap bitmap;
+    Paint paint;
+    Path path;
+    Canvas canvas;
+    float mX, mY;
+    static final float TOLERANCE = 4;
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        path = new Path();
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(4f);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+
+    }
+
+    public void startTouch(float x, float y)
+    {
+        path.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    public void moveTouch(float x, float y)
+    {
+        float dx = Math.abs(x-mX);
+        float dy = Math.abs(y-mY);
+
+        if(dx >= TOLERANCE || dy >= TOLERANCE)
+        {
+            path.quadTo(mX, mY, (x+mX)/2, (y+mY)/2);
+            mX = x;
+            mY = y;
+
+        }
+    }
+
+    // to clear the canvas
+
+    public void clearCanvas()
+    {
+        path.reset();
+        invalidate();
+    }
+
+    public void upTouch()
+    {
+        path.lineTo(mX, mY);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/sketch_1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sketch_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    CanvasView canvasView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        canvasView = (CanvasView)findViewById(R.id.mycanvas);
+
+    }
+    // View was added through Alt + Enter
+    public void clearCanvas(View v)
+    {
+        canvasView.clearCanvas();
+    }
+}
